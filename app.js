340ea283a5dcac94c09a5dfe20045c8ab5de8017
Amruth Pillai <im.amruth@gmail.com>
2019-01-21 01:47:04 +0530

    connecting to mongoDB through mongoose

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const graphqlHttp = require('express-graphql');
-const { buildSchema } = require('graphql');
+const {
+  buildSchema
+} = require('graphql');
+
+const Event = require('./models/event');
 
 const app = express();
-
-const events = [];
 
 app.use(express.json());
 
@@ -39,21 +42,40 @@ app.use('/graphql', graphqlHttp({
     }
   `),
   rootValue: {
-    events: () => events,
+    events: () => {
+      return Event.find()
+        .then(res => res)
+        .catch((err) => {
+          console.error(err);
+        })
+    },
     createEvent: (args) => {
-      const event = {
-        _id: Math.random().toString(),
+      const event = new Event({
         title: args.eventInput.title,
         description: args.eventInput.description,
-        price: +args.eventInput.price,
-        date: args.eventInput.date,
-      };
+        price: args.eventInput.price,
+        date: new Date(args.eventInput.date),
+      });
 
-      events.push(event);
-      return event;
+      return event
+        .save()
+        .then(res => res._doc)
+        .catch((err) => {
+          console.error(err);
+        });
     },
   },
   graphiql: true,
 }));
 
-app.listen(3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0-ksfag.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`, {
+      useNewUrlParser: true
+    },
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  // eslint-disable-next-line no-console
+  .catch(err => console.error(err));
