7c9dcf45c8b13e16419e5aeb4ece4c7dfbb4fb08
Amruth Pillai <im.amruth@gmail.com>
2019-01-21 01:01:41 +0530

    implementing types and eventInputs in GraphQL

--- app.js
+++ app.js
@@ -4,16 +4,33 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(express.json());
 
 app.use('/graphql', graphqlHttp({
   schema: buildSchema(`
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
     type RootQuery {
-      events: [String!]!
+      events: [Event!]!
     }
 
     type RootMutation {
-      createEvent(name: String): String
+      createEvent(eventInput: EventInput): Event
     }
 
     schema {
@@ -22,10 +39,18 @@ app.use('/graphql', graphqlHttp({
     }
   `),
   rootValue: {
-    events: () => ['Coachella', 'Burning Man', '24 Hour Hackathon'],
+    events: () => events,
     createEvent: (args) => {
-      const eventName = args.name;
-      return eventName;
+      const event = {
+        _id: Math.random().toString(),
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: args.eventInput.date,
+      };
+
+      events.push(event);
+      return event;
     },
   },
   graphiql: true,
