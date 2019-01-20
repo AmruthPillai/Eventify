4cc44b1cfe2afac5bf20a4c6a0254240c3bd372b
Amruth Pillai <im.amruth@gmail.com>
2019-01-21 00:45:58 +0530

    getting started with graphql

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello, Word!');
-});
+app.use('/graphql', graphqlHttp({
+  schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
+
+    type RootMutation {
+      createEvent(name: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => ['Coachella', 'Burning Man', '24 Hour Hackathon'],
+    createEvent: (args) => {
+      const eventName = args.name;
+      return eventName;
+    },
+  },
+  graphiql: true,
+}));
 
 app.listen(3000);
